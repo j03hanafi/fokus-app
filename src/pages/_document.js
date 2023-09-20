3b50c9508c4e2d9a18c9180c97a33f20a88c1249
j03hanafi <namoapak14@gmail.com>
2023-09-20 11:46:07 +0700

    Setup react app

---
+++ src/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+      </body>
+    </Html>
+  )
+}
